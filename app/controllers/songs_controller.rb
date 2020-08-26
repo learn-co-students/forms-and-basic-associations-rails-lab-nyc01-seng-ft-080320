b81261259e60d12987096f2df4e58b05df95b1b4
kain <kaindoneson@gmail.com>
2020-08-26 18:09:31 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,9 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params)
+    
+    @song = Song.create(title: params[:song][:title], genre_id: params[:song][:genre_id], artist_name: params[:song][:artist_name])
+    params[:song][:note_id].each {|id| @song.notes.build(content: id)}
 
     if @song.save
       redirect_to @song
@@ -47,7 +49,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_name, :note_id => [])
   end
 end
 
